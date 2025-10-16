31342ff55c29977cb6c5b8141794428fd7fec5d3
BaselABathish <baselashrawibathish@gmail.com>
2025-10-16 22:04:55 +0200

    fixed

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import requests
+from client import create_client
+
+from search import search
+
+app = FastAPI()
+client = create_client()
+
+
+@app.get('/')
+def confirm_connection():
+    return {"Status": "Active"}
+
+@app.get("/search/{company_name}")
+def search_companies(company_name: str):
+    r = search(company_name)
+    print(type(r))
+    return {"Results": r}
+
+
+
+
